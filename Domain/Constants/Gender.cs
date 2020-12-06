d9f8e81abdc0244b1889856eee16b41e986b6dd0
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-12-06 16:30:57 +0200

    Domain constants.

---
+++ Domain/Constants/Gender.cs
@@ -0,0 +1,9 @@
+﻿namespace CV.Ads_Client.Domain.Constants
+{
+    public enum Gender
+    {
+        NotSpecified,
+        Male,
+        Female
+    }
+}
---
+++ Domain/Constants/SmartDeviceMode.cs
@@ -0,0 +1,9 @@
+﻿namespace CV.Ads_Client.Domain.Constants
+{
+    public enum SmartDeviceMode
+    {
+        Inactive,
+        Active,
+        Blocked
+    }
+}
