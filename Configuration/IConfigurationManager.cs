b1db0dcfc6395a44a4d5f5e3aa07dbcb97e56c6f
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-12-06 16:30:29 +0200

    Configuration manager.

---
+++ Configuration/IConfigurationManager.cs
@@ -0,0 +1,11 @@
+﻿using CV.Ads_Client.Configuration.Model;
+using System;
+
+namespace CV.Ads_Client.Configuration
+{
+    public interface IConfigurationManager
+    {
+        public T RetreiveConfiguration<T>(Func<ConfigurationModel, T> retreiveFunction);
+        public void UpdateConfiguration(Action<ConfigurationModel> updateFunction);
+    }
+}
---
+++ Configuration/LocalJsonFileConfigurationManager.cs
@@ -0,0 +1,50 @@
+﻿using CV.Ads_Client.Configuration.Model;
+using System;
+using System.IO;
+using System.Text.Json;
+
+namespace CV.Ads_Client.Configuration
+{
+    class LocalJsonFileConfigurationManager: IConfigurationManager
+    {
+        private readonly string _congigurationFilePath = "appsettings.json";
+
+        public LocalJsonFileConfigurationManager()
+        { }
+
+        public LocalJsonFileConfigurationManager(string congigurationFilePath)
+        {
+            if (!File.Exists(congigurationFilePath))
+            {
+                throw new ArgumentException($"The file '{congigurationFilePath}' doesn't exist.");
+            }
+            _congigurationFilePath = congigurationFilePath;
+        }
+
+        public T RetreiveConfiguration<T>(Func<ConfigurationModel, T> retreiveFunction)
+        {
+            ConfigurationModel configuration = RetreiveConfigurationModel();
+            return retreiveFunction(configuration);
+        }
+
+        public void UpdateConfiguration(Action<ConfigurationModel> updateFunction)
+        {
+            ConfigurationModel configuration = RetreiveConfigurationModel();
+            updateFunction(configuration);
+
+            string configurationUpdatedJson = JsonSerializer.Serialize(configuration, GetJsonOptions());
+            File.WriteAllText(_congigurationFilePath, configurationUpdatedJson);
+        }
+
+        private ConfigurationModel RetreiveConfigurationModel()
+        {
+            string configurationJson = File.ReadAllText(_congigurationFilePath);
+            var configuration = JsonSerializer.Deserialize<ConfigurationModel>(configurationJson, GetJsonOptions());
+
+            return configuration;
+        }
+
+        private JsonSerializerOptions GetJsonOptions() =>
+            new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+    }
+}
