26fc69ed0be440bc1cccb92554fcc0f9d03109b4
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-12-06 16:28:27 +0200

    Cipher service.

---
+++ Services/Implementations/CaesarCipherService.cs
@@ -0,0 +1,23 @@
+﻿using CV.Ads_Client.Services.Interfaces;
+using System;
+using System.Linq;
+
+namespace CV.Ads_Client.Services.Implementations
+{
+    public class CaesarCipherService : ICipherService
+    {
+        private const int OFFSET = 5;
+
+        public string EncodeString(string originalString)
+        {
+            char[] encodedCharArray = originalString.Select(c => Convert.ToChar(c + OFFSET)).ToArray();
+            return new string(encodedCharArray);
+        }
+
+        public string DecodeString(string encodedString)
+        {
+            char[] originalString = encodedString.Select(c => Convert.ToChar(c - OFFSET)).ToArray();
+            return new string(originalString);
+        }
+    }
+}
---
+++ Services/Interfaces/ICipherService.cs
@@ -0,0 +1,8 @@
+﻿namespace CV.Ads_Client.Services.Interfaces
+{
+    public interface ICipherService
+    {
+        public string EncodeString(string originalString);
+        public string DecodeString(string encodedString);
+    }
+}
