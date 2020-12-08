ed535976edc96bddf128c25eef529d10e729a871
Ihor Tsoi <ihor.tsoi@nure.ua>
2020-12-09 01:50:51 +0200

    Create NomacsImageDisplayer.cs

---
+++ Services/Implementations/NomacsImageDisplayer.cs
@@ -0,0 +1,24 @@
+﻿using CV.Ads_Client.Services.Interfaces;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace CV.Ads_Client.Services.Implementations
+{
+    public class NomacsImageDisplayer : IImageDisplayer
+    {
+        public void Display(string imagePath, int durationInSeconds)
+        {
+            if (!File.Exists(imagePath))
+            {
+                throw new ArgumentException("The path to file is invalid.");
+            }
+
+            using var showImageProcess = Process.Start(
+                @"C:\Program Files\nomacs\bin\nomacs.exe", "--fullscreen " + imagePath);
+            Task.Delay(durationInSeconds * 1000).Wait();
+            showImageProcess.Kill();
+        }
+    }
+}
